8a36c956231ff7cd2f02efcc4adcc25efc285886
Michael Von Hippel <vonhippelscienceandenterprises@gmail.com>
2016-01-30 16:41:56 -0800

    mobed sram files

---
+++ sram.c
@@ -0,0 +1,17 @@
+#include "bus.h"
+#include "mcc_generated_files/pin_manager.h"
+
+/*
+SRAM control signals are active low
+*/
+
+// Reads one byte of data and returns it. 
+uint8_t readSRAM(uint8_t addr) {
+	uint8_t data;
+	return data;
+}
+
+// Writes one byte of data to addr in SRAM
+void writeSRAM(uint8_t addr, uint8_t data) {
+	
+}
