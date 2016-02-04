06e827f12b6cf27ea82579a265a2c2706b34b97e
David Orchard <dlo9@uw.edu>
2016-02-03 19:48:56 -0800

    spi master-slave functions done (not tested)

--- Lab2Slave/network.c
+++ Lab2Slave/network.c
@@ -16,6 +16,7 @@ uint8_t data_write[SPI_LEN];
 uint8_t data_read[SPI_LEN];
 
 #define SLAVE_ADDR_MASK 0b11100000
+#define SLAVE_ADDR_SHIFT 5
 #define DATATYPE_MASK 0b00011111
 
 // MASTER FUNCTIONS
@@ -24,7 +25,7 @@ uint8_t data_read[SPI_LEN];
 // in the format:
 //   Slave Address [2:0] : Datatype [4:0] : SRAM Address [7:0]
 uint16_t requestFromSlave(uint8_t slave_addr, enum DataType datatype, uint8_t sram_addr) {
-    data_write[0] = SLAVE_ADDR_MASK & (slave_addr << 5);
+    data_write[0] = SLAVE_ADDR_MASK & (slave_addr << SLAVE_ADDR_SHIFT);
     data_write[0] &= DATATYPE_MASK & (uint8_t)datatype;
     data_write[1] = sram_addr;
     
@@ -33,7 +34,6 @@ uint16_t requestFromSlave(uint8_t slave_addr, enum DataType datatype, uint8_t sr
     do
     {
         total = SPI1_Exchange8bitBuffer(&data_write[total], SPI_LEN - total, &data_read[total]);
-
     } while(total < SPI_LEN);
     
     // Read phase
@@ -46,21 +46,39 @@ uint16_t requestFromSlave(uint8_t slave_addr, enum DataType datatype, uint8_t sr
     do
     {
         total = SPI1_Exchange8bitBuffer(&data_write[total], SPI_LEN - total, &data_read[total]);
-
     } while(total < SPI_LEN);
        
     return (uint16_t)((data_read[0] << 8) & data_read[1]);
 }
 
 // SLAVE FUNCTIONS
+// data will be multiplied by the slave_address (1-6)
+// so must be between 0-10922
+uint16_t slave_data[] = 
+    {
+        0xFFFF, // NONE
+        4, // FREQ_HIGH
+        8, // FREQ_LOW, 
+        16, // PERIOD_HIGH, 
+        32, // PERIOD_LOW, 
+        64, // COUNT_HIGH, 
+        128, // COUNT_LOW, 
+        256, // INTERVAL_HIGH,
+        512, // INTERVAL_LOW,
+        1024, // ANALYSIS,
+        2048, // SRAM
+    };
 
 uint16_t getDataForMaster(uint8_t slave_addr, enum DataType datatype, uint8_t sram_addr) {
-    return 0;
+    // Invalid things
+    if (slave_addr > 5 || datatype == NONE || sram_addr > 15)
+        return 0xFFFF;
+    
+    return (slave_addr+1)*slave_data[datatype] + sram_addr;
 }
 
 // waits for communication from the master and acts accordingly
 void listenForMaster() {
-    
     // Read phase
     
     // Clear for sanity
@@ -71,10 +89,11 @@ void listenForMaster() {
     do
     {
         total = SPI1_Exchange8bitBuffer(&data_write[total], SPI_LEN - total, &data_read[total]);
-
     } while(total < SPI_LEN);
     
-    uint16_t data = getDataForMaster();
+    uint16_t data = getDataForMaster(SLAVE_ADDR_MASK & (data_read[0] << SLAVE_ADDR_SHIFT), 
+            DATATYPE_MASK & data_read[0],
+            data_write[1]);
     data_write[0] = (uint8_t) (0xF & (data >> 8));
     data_write[1] = (uint8_t) (0xF & data);
     
@@ -83,6 +102,5 @@ void listenForMaster() {
     do
     {
         total = SPI1_Exchange8bitBuffer(&data_write[total], SPI_LEN - total, &data_read[total]);
-
     } while(total < SPI_LEN);
 }
