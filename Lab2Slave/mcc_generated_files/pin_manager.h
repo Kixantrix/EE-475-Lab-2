a07a0da6e8f26f70a1c321827b37c3a0b29ab85a
David Orchard <dlo9@uw.edu>
2016-02-03 21:28:58 -0800

    Added slave generated files

---
+++ Lab2Slave/mcc_generated_files/pin_manager.h
@@ -0,0 +1,146 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using MPLAB� Code Configurator
+
+  @Description:
+    This header file provides implementations for pin APIs for all pins selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB� Code Configurator - v2.25.2
+        Device            :  PIC18LF25K22
+        Version           :  1.01
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 v1.34
+        MPLAB             :  MPLAB X v2.35 or v3.00
+ */
+
+/*
+Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ */
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set SS1 aliases
+#define SS1_TRIS               TRISA5
+#define SS1_LAT                LATA5
+#define SS1_PORT               PORTAbits.RA5
+#define SS1_ANS                ANSA5
+#define SS1_SetHigh()    do { LATA5 = 1; } while(0)
+#define SS1_SetLow()   do { LATA5 = 0; } while(0)
+#define SS1_Toggle()   do { LATA5 = ~LATA5; } while(0)
+#define SS1_GetValue()         PORTAbits.RA5
+#define SS1_SetDigitalInput()    do { TRISA5 = 1; } while(0)
+#define SS1_SetDigitalOutput()   do { TRISA5 = 0; } while(0)
+
+#define SS1_SetAnalogMode()   do { ANSA5 = 1; } while(0)
+#define SS1_SetDigitalMode()   do { ANSA5 = 0; } while(0)
+// get/set SCK1 aliases
+#define SCK1_TRIS               TRISC3
+#define SCK1_LAT                LATC3
+#define SCK1_PORT               PORTCbits.RC3
+#define SCK1_ANS                ANSC3
+#define SCK1_SetHigh()    do { LATC3 = 1; } while(0)
+#define SCK1_SetLow()   do { LATC3 = 0; } while(0)
+#define SCK1_Toggle()   do { LATC3 = ~LATC3; } while(0)
+#define SCK1_GetValue()         PORTCbits.RC3
+#define SCK1_SetDigitalInput()    do { TRISC3 = 1; } while(0)
+#define SCK1_SetDigitalOutput()   do { TRISC3 = 0; } while(0)
+
+#define SCK1_SetAnalogMode()   do { ANSC3 = 1; } while(0)
+#define SCK1_SetDigitalMode()   do { ANSC3 = 0; } while(0)
+// get/set SDI1 aliases
+#define SDI1_TRIS               TRISC4
+#define SDI1_LAT                LATC4
+#define SDI1_PORT               PORTCbits.RC4
+#define SDI1_ANS                ANSC4
+#define SDI1_SetHigh()    do { LATC4 = 1; } while(0)
+#define SDI1_SetLow()   do { LATC4 = 0; } while(0)
+#define SDI1_Toggle()   do { LATC4 = ~LATC4; } while(0)
+#define SDI1_GetValue()         PORTCbits.RC4
+#define SDI1_SetDigitalInput()    do { TRISC4 = 1; } while(0)
+#define SDI1_SetDigitalOutput()   do { TRISC4 = 0; } while(0)
+
+#define SDI1_SetAnalogMode()   do { ANSC4 = 1; } while(0)
+#define SDI1_SetDigitalMode()   do { ANSC4 = 0; } while(0)
+// get/set SDO1 aliases
+#define SDO1_TRIS               TRISC5
+#define SDO1_LAT                LATC5
+#define SDO1_PORT               PORTCbits.RC5
+#define SDO1_ANS                ANSC5
+#define SDO1_SetHigh()    do { LATC5 = 1; } while(0)
+#define SDO1_SetLow()   do { LATC5 = 0; } while(0)
+#define SDO1_Toggle()   do { LATC5 = ~LATC5; } while(0)
+#define SDO1_GetValue()         PORTCbits.RC5
+#define SDO1_SetDigitalInput()    do { TRISC5 = 1; } while(0)
+#define SDO1_SetDigitalOutput()   do { TRISC5 = 0; } while(0)
+
+#define SDO1_SetAnalogMode()   do { ANSC5 = 1; } while(0)
+#define SDO1_SetDigitalMode()   do { ANSC5 = 0; } while(0)
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    GPIO and peripheral I/O initialization
+ * @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize(void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+ */
